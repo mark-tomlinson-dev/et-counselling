74dc905fdf31e70a679938a8c534cb29072199aa
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-06-18 17:45:25 +1000

    integrated prismic into backend

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+
+const initApi = (req) => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  });
+}
+
+// middleware
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT
+  }
+  res.locals.PrismicDOM = PrismicDOM
+  next()
+})
+
+// pug setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('pages/home')
+// routes
+app.get('/', async (req, res) => {
+  initApi(req).then(api => {
+    api.query(
+      Prismic.Predicates.any('document.type', ['meta', 'header'])) 
+      .then(response => {
+      const { results } = response
+      const [meta, header] = results
+
+      console.log(meta, header)
+
+      res.render('pages/home', {
+        header,
+        meta
+      })
+    })
+  })
 })
 
-app.get('/counselling', (req, res) => {
+app.get('/counselling', async (req, res) => {
   res.render('pages/counselling')
 })
 
