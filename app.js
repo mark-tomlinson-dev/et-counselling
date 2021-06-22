eaafceca156ae4a139c4869f8422f5b7cfd9b4c7
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-06-22 19:33:21 +1000

    Add scss basic architecture

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const logger = require('morgan')
+
+
 const app = express()
 const path = require('path')
 const port = 3000
+
+app.use(errorHandler())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
+
+// Serve our CSS
+app.use(express.static(path.join(__dirname, 'public')))
 
 const Prismic = require('@prismicio/client');
 const PrismicDOM = require('prismic-dom');
@@ -14,12 +29,25 @@ const initApi = (req) => {
   });
 }
 
-// middleware
+const handleLinkResolver = doc => {
+  if (doc.type == 'counselling') {
+    return '/counselling'
+  }
+
+  return '/'
+}
+
+
 app.use((req, res, next) => {
+
+  res.locals.link = handleLinkResolver
+
   res.locals.ctx = {
     endpoint: process.env.PRISMIC_ENDPOINT
   }
+
   res.locals.PrismicDOM = PrismicDOM
+  
   next()
 })
 
