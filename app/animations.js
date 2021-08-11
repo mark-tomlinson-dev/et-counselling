02a3e2ddf98adcf14403e5d16ca347f76d8bad63
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-08-11 17:01:32 +1000

    start overhaul

--- app/animations.js
+++ app/animations.js
@@ -1,36 +1,36 @@
-let imageItems = [...document.querySelectorAll('.intro__media')]
-console.log(imageItems)
+// let imageItems = [...document.querySelectorAll('.intro__media')]
+// console.log(imageItems)
 
-let textItems = [...document.querySelectorAll('.intro__text')]
-console.log(textItems)
+// let textItems = [...document.querySelectorAll('.intro__text')]
+// console.log(textItems)
 
-let titleItems = [...document.querySelectorAll('.intro__title')]
+// let titleItems = [...document.querySelectorAll('.intro__title')]
 
 
 
-// When the element is 150px in the viewport, trigger the intersection obs callback
-let options = {
-  rootMargin: '-80px'
-}
+// // When the element is 150px in the viewport, trigger the intersection obs callback
+// let options = {
+//   rootMargin: '-80px'
+// }
 
-let setItemActive = (entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('active')
-    }
-  })
-})
+// let setItemActive = (entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('active')
+//     }
+//   })
+// })
 
-let observer = new IntersectionObserver(setItemActive, options);
+// let observer = new IntersectionObserver(setItemActive, options);
 
-imageItems.forEach((item, idx) => {
-  observer.observe(item, idx)
-})
+// imageItems.forEach((item, idx) => {
+//   observer.observe(item, idx)
+// })
 
-textItems.forEach((item, idx) => {
-  observer.observe(item, idx)
-})
+// textItems.forEach((item, idx) => {
+//   observer.observe(item, idx)
+// })
 
-titleItems.forEach((item, idx) => {
-  observer.observe(item, idx)
-})
+// titleItems.forEach((item, idx) => {
+//   observer.observe(item, idx)
+// })
