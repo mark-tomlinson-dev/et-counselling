29feef363fedf3d707133c705a6c97ca35065915
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-09-01 17:00:07 +1000

    add preloader

--- app/animations.js
+++ app/animations.js
@@ -34,7 +34,7 @@ gsap.to('.intro__text-content-text *', {
 })
 
 
-const timeline = gsap.timeline({scrollTrigger: {
+gsap.timeline({scrollTrigger: {
   trigger: ".about",
   start: "top 70%",
   end: "+=400",
@@ -61,6 +61,86 @@ const timeline = gsap.timeline({scrollTrigger: {
 
 
 
+
+
+
+
+
+
+
+// let isLoadingAnimationEnd = false;
+
+// const maskAnimation = () => {
+//   const tl = gsap.timeline();
+//   const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
+//   const end = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
+//   gsap.set(".mask", { autoAlpha: 1 });
+//   tl.to(".path", {
+//     duration: 0.8,
+//     attr: { d: start },
+//     ease: "power2.in"
+//   }).to(".path", { duration: 0.4, attr: { d: end }, ease: "power2.out" });
+
+//   return tl;
+// };
+
+// const entranceAnimation = () => {
+//   const tl = gsap.timeline();
+//   tl.add(maskAnimation())
+//     .add(loadingAnimationOut(), 0.2)
+//     .to(
+//       "#main-content",
+//       {
+//         opacity: 1,
+//         duration: 0.3
+//       },
+//       0.8
+//     )
+//     .from(
+//       "#main-content",
+//       {
+//         duration: 3,
+//         opacity: 0,
+//         stagger: 0.2,
+//         ease: "power2.out"
+//       },
+//       0.8
+//     );
+// };
+
+// const loadingAnimationOut = () => {
+//   const tl = gsap.timeline();
+//   tl.to(".splash-title", {
+//     y: -window.innerHeight,
+//     duration: 1.3,
+//     ease: "power2.inOut",
+//     opacity: 0
+//   });
+
+//   return tl;
+// };
+
+// const loadingAnimation = () => {
+//   const tl = gsap
+//     .timeline({
+//       onComplete: () => {
+//         isLoadingAnimationEnd = true;
+//         entranceAnimation();
+//       }
+//     })
+//     tl.from(
+//       ".splash-title",
+//       {
+//         opacity: 1,
+//         duration: 1,
+//         ease: "power2.out"
+//       },
+//       0.5
+//     );
+// };
+
+// loadingAnimation();
+// if (isLoadingAnimationEnd) entranceAnimation();
 
 
 
