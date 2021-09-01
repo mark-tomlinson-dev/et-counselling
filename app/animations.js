6db4b13bcf0a155392bea766a4aafc406820d996
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-09-01 17:38:39 +1000

    make gsap timelines responsive

--- app/animations.js
+++ app/animations.js
@@ -2,62 +2,69 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".intro__text-content-text", {
-  x: 0,
-  y: 0,
-  scrollTrigger: {
-    trigger: '.intro__text-content-text',
-    start: 'top 80%',
-    end: 'bottom 75%',
-    scrub: true,
+ScrollTrigger.saveStyles(".intro__text-content-title, .intro__text-content-text *, .text, .title")
+ScrollTrigger.matchMedia({
+
+  "(max-width: 768px)": function() {
+    gsap.to(".intro__text-content-text", {
+      x: 0,
+      y: 0,
+      scrollTrigger: {
+        trigger: '.intro__text-content-text',
+        start: 'top 80%',
+        end: 'bottom 75%',
+        scrub: true,
+      }
+    })
+
+    gsap.to('.intro__text-content-title', {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.intro__text-content-text',
+        start: 'top 60%',
+        end: 'bottom 60%',
+        scrub: true
+      }
+    })
+
+    gsap.to('.intro__text-content-text *', {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.intro__text-content-text',
+        start: 'top 40%',
+        end: 'bottom 65%',
+        scrub: true
+      }
+    })
+    gsap.timeline({scrollTrigger: {
+      trigger: ".about",
+      start: "top 70%",
+      end: "+=400",
+      toggleActions: "restart none none reverse",
+      scrub: 0.5,
+      ease: "sine.out",
+    }})
+    
+    .to(".montage-adoption-image", {
+      y: -30,
+      opacity: 1
+    })
+    .to(".montage-intro-image", {
+      y: -60,
+      opacity: 1
+    }, 0)
+    .to(".montage-about-image", {
+      y: -120,
+      opacity: 1
+    }, 0)
+    .to(".montage__orange__ribbon", {
+      opacity: 1
+    }, 0.2)
   }
 })
 
-gsap.to('.intro__text-content-title', {
-  opacity: 1,
-  scrollTrigger: {
-    trigger: '.intro__text-content-text',
-    start: 'top 60%',
-    end: 'bottom 60%',
-    scrub: true
-  }
-})
-
-gsap.to('.intro__text-content-text *', {
-  opacity: 1,
-  scrollTrigger: {
-    trigger: '.intro__text-content-text',
-    start: 'top 40%',
-    end: 'bottom 65%',
-    scrub: true
-  }
-})
 
 
-gsap.timeline({scrollTrigger: {
-  trigger: ".about",
-  start: "top 70%",
-  end: "+=400",
-  toggleActions: "restart none none reverse",
-  scrub: 0.5,
-  ease: "sine.out",
-}})
-
-.to(".montage-adoption-image", {
-  y: -30,
-  opacity: 1
-})
-.to(".montage-intro-image", {
-  y: -60,
-  opacity: 1
-}, 0)
-.to(".montage-about-image", {
-  y: -120,
-  opacity: 1
-}, 0)
-.to(".montage__orange__ribbon", {
-  opacity: 1
-}, 0.2)
 
 
 
